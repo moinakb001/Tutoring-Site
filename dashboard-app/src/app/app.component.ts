26cc0900cd35a97f156d58fc6b4b822979d0109a
Moinak Bhattacharyya <mb.totaldomination@gmail.com>
2016-06-24 19:21:46 -0700

    Initial Commit

---
+++ dashboard-app/src/app/app.component.ts
@@ -0,0 +1,6 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'my-app',
+  template: '<h1>My First Angular 2 App HA!<button class="btn btn-block btn-lg btn-primary">HA!</button></h1>'
+})
+export class AppComponent { }
---
+++ dashboard-app/src/vendor.ts
@@ -0,0 +1,14 @@
+// Angular 2
+import '@angular/platform-browser';
+import '@angular/platform-browser-dynamic';
+import '@angular/core';
+import '@angular/common';
+import '@angular/http';
+import '@angular/router-deprecated';
+// RxJS
+import 'rxjs';
+//Bootstrap
+import 'bootstrap/dist/css/bootstrap.css';
+//import 'bootstrap/dist/css/bootstrap-theme.css';
+import 'bootstrap/dist/js/bootstrap.js';
+import 'flat-ui/dist/css/flat-ui.css'
